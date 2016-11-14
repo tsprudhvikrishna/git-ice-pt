dc20341c68f3b378abd84b9cd38da00368a6411b
Tarugu Subbaiah,Prudvi Krishna <s525769@ch140033m.nwmissouri.edu>
2016-11-14 16:45:58 -0600

    I have added some changes in view controller

--- Git-ICE-PT/BeeVC.swift
+++ Git-ICE-PT/BeeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class BeeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Git-ICE-PT/EhVC.swift
+++ Git-ICE-PT/EhVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class EhVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
